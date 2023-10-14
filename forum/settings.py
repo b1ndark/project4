f0f6cf6c7c4fd184d4c9095f8b4a465408629a98
b1ndark <>
2023-10-14 15:16:43 +0000

    add Post to admin, install summernote & add to urls

--- forum/settings.py
+++ forum/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'cloudinary',
 
     # Others
-
+    'django_summernote',
 
     # Apps
     'typerforum',
@@ -136,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'cloudinary_storage.StaticHashedCloudinaryStorage'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
--- typerforum/models.py
+++ typerforum/models.py
@@ -11,12 +11,12 @@ STATUS = ((0, 'Draft'), (1, 'Published'))
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
+    featured_image = CloudinaryField('image', default='placeholder')
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='forum_posts')
     content = models.TextField()
     excerpt = models.TextField()
-    featured_image = CloudinaryField('image', default='placeholder')
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
