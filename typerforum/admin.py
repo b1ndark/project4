36199be63d1fc118c73f77b7b7b817d77d092c5f
b1ndark <>
2023-10-14 16:06:14 +0000

    add postadmin model

--- typerforum/admin.py
+++ typerforum/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'status', 'created_on')
+    search_fields = ['title', 'content', 'car_model']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on', 'likes')
+    summernote_fields = ('content')
