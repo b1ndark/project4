257394315d1200e193d9229cec9278601c77fa2c
b1ndark <>
2023-10-15 21:52:31 +0000

    add forum.html and connect it

--- typerforum/views.py
+++ typerforum/views.py
@@ -7,7 +7,11 @@ def home(request):
     return render(request, 'index.html')
 
 
+def forum(request):
+    return render(request, 'forum.html')
+
+
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
+    template_name = 'forum.html'
