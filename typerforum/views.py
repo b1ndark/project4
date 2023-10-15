1a13c1be2074f5cd50eb6af78563448ee1c14d72
b1ndark <>
2023-10-15 18:00:11 +0000

    add urls.py to typerforum and connect it to forum urls

--- typerforum/views.py
+++ typerforum/views.py
@@ -6,4 +6,4 @@ from .models import Post, Comment
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'base.html'
+    template_name = 'index.html'
