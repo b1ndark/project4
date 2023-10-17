1c7d590407e68e29006ad1abeafa6bcb6d07b34c
b1ndark <>
2023-10-17 21:39:24 +0000

    add structure to forum_details and update urls

--- typerforum/urls.py
+++ typerforum/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.home, name='home'),
     path('forum/', views.PostList.as_view(), name="forum"),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='forum_detail'),
 ]
