93b499f57d21863685741c26759dd13c63e07c1c
b1ndark <>
2023-10-17 20:36:01 +0000

    add forum_detail.html, add PostDetail class in views, fix bug 'likes'

--- forum/settings.py
+++ forum/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-b1ndark-project4-sx67lbxq25.us2.codeanyapp.com',
                  'typerforum-fb5dd209c30e.herokuapp.com']
