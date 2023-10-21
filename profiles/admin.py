528b64dde9966d34874ac1e520a270c46f453a45
b1ndark <>
2023-10-21 21:25:44 +0000

    update profiles admin.py file

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'car_model',
+        'country',
+    )
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
