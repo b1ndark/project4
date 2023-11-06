ad9bde63261ec9639e15e6367b8833a201a775ba
b1ndark <>
2023-11-06 19:56:48 +0000

    add style, update comment layout(html, models, forms), migrate contact

--- contact/forms.py
+++ contact/forms.py
@@ -1,11 +1,15 @@
 from django import forms
 from .models import UserContact
+from typerforum.models import CAR_MODELS
+from django.db import models
 
 
 class ContactForm(forms.ModelForm):
     """
     To display the Contact form
     """
+    car_model = models.CharField(
+        max_length=30, choices=CAR_MODELS, default="Select Car Model")
 
     class Meta:
         model = UserContact
--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.shortcuts import reverse
+from typerforum.models import CAR_MODELS
 
 
 class UserContact(models.Model):
@@ -10,7 +11,8 @@ class UserContact(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     subject = models.CharField(max_length=200)
-    car_model = models.CharField(max_length=200)
+    car_model = models.CharField(
+        max_length=30, choices=CAR_MODELS, default="")
     message_content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
