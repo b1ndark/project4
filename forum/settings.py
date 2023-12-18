f2c77aa1e6b67317196537ef4ed01a6ffa1a41a4
b1ndark <>
2023-12-18 19:43:40 +0000

    add flags, update settings and htmls

--- forum/settings.py
+++ forum/settings.py
@@ -67,8 +67,6 @@ INSTALLED_APPS = [
     'contact',
 ]
 
-COUNTRIES_FLAG_URL = ''
-
 SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
@@ -174,7 +172,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
